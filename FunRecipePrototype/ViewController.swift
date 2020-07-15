e581b5dca4840cf26c13a2fb8dcd5138922f3da9
kenta.takano <san.kun.sakana@gmail.com>
2020-07-15 13:13:13 +0900

    Initial Commit

---
+++ FunRecipePrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunRecipePrototype
+//
+//  Created by  ohayoukenchan on 2020/07/15.
+//  Copyright © 2020  ohayoukenchan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
